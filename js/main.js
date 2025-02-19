ae7f3bd5ee8587297072021661873534a1b1196a
MiraVoronina <voroninaslava23@gmail.com>
2025-02-19 11:49:52 +0700

    form

--- js/main.js
+++ js/main.js
@@ -1,60 +1,99 @@
-let app = new Vue({
-    el: '#app',
-    data: {
-        product: "Socks",
-        brand: 'Vue Mastery',
-        selectedVariant: 0,
-        altText: "A pair of socks",
-        onSale: true,
-        details: ['80% cotton', '20% polyester', 'Gender-neutral'],
-        variants: [
-            {
+Vue.component('product', {
+    props: {premium: {type: Boolean, required: true}},
+    template: `
+      <div class="product">
+        <div class="product-image"><img :src="image" :alt="altText"/></div>
+        <div class="product-info"><h1>{{ title }}</h1>
+          <p v-if="inStock">In stock</p>
+          <p v-else>Out of Stock</p>
+          <ul>
+            <li v-for="detail in details">{{ detail }}</li>
+          </ul>
+          <p>Shipping: {{ shipping }}</p>
+          <div class="color-box" v-for="(variant, index) in variants" :key="variant.variantId"
+               :style="{ backgroundColor:variant.variantColor }" @mouseover="updateProduct(index)"></div>
+          <button v-on:click="addToCart" :disabled="!inStock" :class="{ disabledButton: !inStock }"> Add to cart
+          </button>
+          <product-review @review-submitted="addReview"></product-review>
+        </div>
+      </div>  `,
+    data() {
+        return {
+            product: "Socks",
+            brand: 'Vue Mastery',
+            selectedVariant: 0,
+            altText: "A pair of socks",
+            details: ['80% cotton', '20% polyester', 'Gender-neutral'],
+            variants: [{
                 variantId: 2234,
                 variantColor: 'green',
                 variantImage: "./assets/vmSocks-green-onWhite.jpg",
-                variantQuantity: 10 // Убедитесь, что количество больше 0
-            },
-            {
+                variantQuantity: 10
+            }, {
                 variantId: 2235,
                 variantColor: 'blue',
                 variantImage: "./assets/vmSocks-blue-onWhite.jpg",
-                variantQuantity: 0 // Этот вариант не в наличии
+                variantQuantity: 0
             }
-        ],
-        cart: 0,
-        inStock: true // Установите значение true, чтобы указать, что товар в наличии
+            ],
+        }
     },
     methods: {
         addToCart() {
-            this.cart += 1;
-        },
-        updateProduct(index) {
+            this.$emit('add-to-cart', this.variants[this.selectedVariant].variantId);
+        }, updateProduct(index) {
             this.selectedVariant = index;
             console.log(index);
-        },
-        removeItemFromCart(id) {
-            const index = this.cart.indexOf(id);
-            if (index !== -1) {
-                this.cart.splice(index, 1);
-            }
         }
     },
     computed: {
         title() {
             return this.brand + ' ' + this.product;
-        },
-        image() {
+        }, image() {
             return this.variants[this.selectedVariant].variantImage;
-        },
-        inStock() {
-            return this.variants[this.selectedVariant].variantQuantity > 0;
-        },
-        shipping() {
+        }, inStock() {
+            return this.variants[this.selectedVariant].variantQuantity
+        }, shipping() {
             if (this.premium) {
                 return "Free";
             } else {
-                return "$2.99";
+                return 2.99
             }
         }
     }
-});
+})
+Vue.component('product-review', {
+    template: `
+      <form class="review-form" @submit.prevent="onSubmit"><p><label for="name">Name:</label> <input id="name"
+                                                                                                     v-model="name"
+                                                                                                     placeholder="name">
+      </p>
+        <p><label for="review">Review:</label> <textarea id="review" v-model="review"></textarea></p>
+        <p><label for="rating">Rating:</label> <select id="rating" v-model.number="rating">
+          <option>5</option>
+          <option>4</option>
+          <option>3</option>
+          <option>2</option>
+          <option>1</option>
+        </select></p>
+        <p><input type="submit" value="Submit"></p>
+        Vue.js
+      </form>   `, data() {
+        return {name: null, review: null, rating: null}
+    },
+    methods: {
+        onSubmit() {
+            let productReview = {name: this.name, review: this.review, rating: this.rating}
+            this.$emit('review-submitted', productReview)
+            this.name = null
+            this.review = null
+            this.rating = null
+        }}
+    })
+    let app = new Vue({
+        el: '#app', data: {premium: true, cart: []}, methods: {
+            updateCart(id) {
+                this.cart.push(id);
+            }
+        }
+    })
